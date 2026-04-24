a2b10624e88e1b1548e132ceaa9bde4cee316b46
GNcode <gngalarga027@gmail.com>
2026-04-24 09:20:44 -0500

    2.8: La Agenda Telefónica Paso 3

--- part2/phoneagenda/src/App.jsx
+++ part2/phoneagenda/src/App.jsx
@@ -2,12 +2,16 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas', number: '234-234234' }
   ])
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('');
 
   const handleNewName = e => {
     setNewName(e.target.value)
+  }
+  const handleNewNumber = e => {
+    setNewNumber(e.target.value)
   }
 
   const addNumber = e => {
@@ -19,12 +23,14 @@ const App = () => {
       return;
     }
 
-    const newNumber = {
-      name: newName
+    const newContact = {
+      name: newName,
+      number: newNumber
     }
 
-    setPersons([...persons].concat(newNumber));
+    setPersons([...persons].concat(newContact));
     setNewName('');
+    setNewNumber('')
   }
 
   return (
@@ -36,13 +42,16 @@ const App = () => {
           name: <input value={newName} onChange={handleNewName} />
         </div>
         <div>
+          number: <input value={newNumber} onChange={handleNewNumber} />
+        </div>
+        <div>
           <button type="submit">add</button>
         </div>
       </form>
 
       <h2>Numbers</h2>
       <div>
-        {persons.map(person => <p key={person.name}>{person.name}</p>)}
+        {persons.map(person => <p key={person.name}>{person.name} {person.number}</p>)}
       </div>
     </div>
   )
