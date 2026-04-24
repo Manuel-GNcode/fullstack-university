84328ed9c82dd6e26d7a04a613b8f1c8271dce34
GNcode <gngalarga027@gmail.com>
2026-04-24 09:05:18 -0500

    2.6: La Agenda Telefónica Paso 1

---
+++ part2/phoneagenda/src/App.jsx
@@ -0,0 +1,45 @@
+import { useState } from 'react'
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ])
+  const [newName, setNewName] = useState('')
+
+  const handleNewName = e => {
+    setNewName(e.target.value)
+  }
+
+  const addNumber = e => {
+    e.preventDefault()
+
+    const newNumber = {
+      name: newName
+    }
+
+    setPersons([...persons].concat(newNumber));
+    setNewName('');
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+
+      <form onSubmit={addNumber}>
+        <div>
+          name: <input value={newName} onChange={handleNewName} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+
+      <h2>Numbers</h2>
+      <div>
+        {persons.map(person => <p key={person.name}>{person.name}</p>)}
+      </div>
+    </div>
+  )
+}
+
+export default App
